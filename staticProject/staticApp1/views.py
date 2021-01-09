b17e7fd0917c18453c51e46c1cf5d74990a03198
Eshwari Patil <eshwaripatil31533@gmail.com>
2021-01-09 22:16:51 +0530

    my first commit

---
+++ staticProject/staticApp1/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def view1(request):
+    myName="Rahul"
+    favPlayer="Dhoni"
+    favAnimal="Lion"
+    favSubject="python"
+    d={'name':myName,'player':favPlayer,'subject':favSubject,'animal':favAnimal}
+    return render(request,'staticApp1/1.html',d)
